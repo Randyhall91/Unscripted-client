a3b7ecf09afbbd09943f2410db6e1e43c303bc98
Randyhall91 <randyrocks11@gmail.com>
2022-12-07 22:23:39 -0700

    adjust env.js to vercel .env

--- src/env.js
+++ src/env.js
@@ -2,4 +2,8 @@ export const dev = window.location.origin.includes('localhost')
 export const baseURL = process.env.baseURL
 
 export const storageURL = process.env.storageURL
+export const useSockets = false
+export const domain = ''
+export const clientId = ''
+export const audience = ''
 export const bearer = process.env.bearer
--- src/services/AxiosService.js
+++ src/services/AxiosService.js
@@ -1,5 +1,5 @@
 import Axios from 'axios'
-import { baseURL, bearer } from '../.env.js'
+import { baseURL, bearer } from '../env'
 export const api = Axios.create({
   baseURL,
   timeout: 8000,
