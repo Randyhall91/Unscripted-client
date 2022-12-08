31f045fbbdfc3266ca3761492431ed26478e4dc1
Randyhall91 <randyrocks11@gmail.com>
2022-12-07 19:52:33 -0700

    push new env for vercel

---
+++ src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = process.env.baseURL
+
+export const storageURL = process.env.storageURL
+export const bearer = process.env.bearer
