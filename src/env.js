9f5536b0df5c71aa6a9488a099866f1e15bcf7cd
Randy <48418003+Randyhall91@users.noreply.github.com>
2022-12-07 19:38:01 -0700

    Delete .env
    It shouldn't be here...

--- src/env.js
+++
@@ -1,5 +0,0 @@
-export const dev = window.location.origin.includes('localhost')
-export const baseURL = 'http://localhost:1337'
-
-export const storageURL = 's3.us-west-2.amazonaws.com/unscriptedfamilyphotography'
-export const bearer = 'eb62069a062232f6ff1905514a09e142b4f34db40c493a9ca7838003387c1367fea2bc1341fdad1cb87f78ac08e1748b1bd00b5d0add90463a78046ceb281b10c2645e807a8575701d68f8a9952738f77f6ee7d53936cdc80eca22501d29bf7175cacdf911671dbc42619a561a5784df2d1d3a2b2a28e695e69ff6eb912bf12f'
