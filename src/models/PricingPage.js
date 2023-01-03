668ab7e51c8562ff5b0b8df1bafb2e1d49bdedc8
Thesommoner <randyrocks11@gmail.com>
2023-01-02 20:52:05 -0700

    pricing review

--- src/models/PricingPage.js
+++ src/models/PricingPage.js
@@ -7,5 +7,8 @@ export class PricingPage{
     this.description = data.description
     this.descriptionImg = data.descripitionImg.data.attributes.formats.medium.url
     this.session = data.session
+    this.review = data.review
+    this.reviewName = data.reviewName
+    this.reviewImg= data.reviewImg.data.attributes.formats.medium.url
   }
 }
