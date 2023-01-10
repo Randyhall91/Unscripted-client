c9e2fecc5c0f1c2afbf557634d8342cb5237040f
Thesommoner <randyrocks11@gmail.com>
2023-01-09 20:17:28 -0700

    pricing content

--- src/pages/PricingPage.vue
+++ src/pages/PricingPage.vue
@@ -22,84 +22,8 @@
       </div>
     </div>
         <!-- TODO reinsert  v-html="pricing.session" -->
-      
-    <div class="row">
-      <div class="col-12 text-center mt-5">
-        <h1><strong>Lifestyle Family Session Packages</strong></h1>
-      </div>
-    </div>
-    <div class="row d-flex justify-content-center">
-      <div class="col-lg-6 text-center mt-3">
-        <h3><strong>---------Gotta Have it------------</strong></h3>
-        <h5>This package is perfect for the family that has a wall to display their photos and needs to have coffee table art.</h5>
-        <h3><strong>$955</strong></h3>
-      </div>
-    </div>
-    <div class="row d-flex justify-content-center mt-2">
+      <div v-html="pricing.session"></div>
 
-      <div class="col-lg-6">
-        <ul>
-        <li class="ps-2 fs-5"> Get to know each other phone call </li>
-        <li class="ps-2 fs-5"> Help with family outfits </li>
-        <li class="ps-2 fs-5"> Location scouting</li>
-        <li class="ps-2 fs-5">45-60-minute photo session</li>
-        <li class="ps-2 fs-5">40 edited digital images </li>
-        <li class="ps-2 fs-5"> Hard cover family heritage album</li>
-        <li class="ps-2 fs-5">48x48 4 photo canvas prints for wall art</li>
-        <li class="ps-2 fs-5"> Online gallery within 2 weeks and other art delivery within 60 days</li>
-
-        </ul>
-      </div>
-    </div>
-    <div class="row d-flex justify-content-center">
-      <div class="col-lg-6 text-center mt-3">
-        <h3><strong>------------Love it------------</strong></h3>
-        <h5>This package is perfect for the family that wants to have a beautiful custom heritage album to treasure now and to gift their kids in the years to come. </h5>
-         <h3><strong>$725</strong></h3>
-      </div>
-    </div>
-    <div class="row d-flex justify-content-center mt-2">
-
-      <div class="col-lg-6">
-        <ul>
-        <li class="ps-2 fs-5"> Get to know each other phone call </li>
-        <li class="ps-2 fs-5"> Help with family outfits </li>
-        <li class="ps-2 fs-5"> Location scouting</li>
-        <li class="ps-2 fs-5">45-60-minute photo session</li>
-        <li class="ps-2 fs-5">40 edited digital images </li>
-        <li class="ps-2 fs-5"> Hard cover family heritage album</li>
-        <li class="ps-2 fs-5"> Online gallery within 2 weeks and album delivery within 60 days</li>
-
-        </ul>
-      </div>
-    </div>
-    <div class="row d-flex justify-content-center">
-      <div class="col-lg-6 text-center mt-3">
-        <h3><strong>--------------Like it---------------</strong></h3>
-        <h5>This package is perfect for the family that wants to have a beautiful custom heritage album to treasure now and to gift their kids in the years to come. e is perfect for the family that wants to have a beautiful custom heritage album to treasure now and to gift their kids in the years to come. </h5>
-        <h3><strong>$650</strong></h3>
-      </div>
-    </div>
-    <div class="row d-flex justify-content-center mt-2">
-
-      <div class="col-lg-6">
-        <ul>
-        <li class="ps-2 fs-5"> Get to know each other phone call </li>
-        <li class="ps-2 fs-5"> Help with family outfits </li>
-        <li class="ps-2 fs-5"> Location scouting</li>
-        <li class="ps-2 fs-5">45-60-minute photo session</li>
-        <li class="ps-2 fs-5">40 edited digital images </li>
-        <li class="ps-2 fs-5"> Hard cover family heritage album</li>
-        <li class="ps-2 fs-5"> Online gallery within 2 weeks </li>
-      </ul>
-    <div class="row d-flex justify-content-center">
-      <div class="col-lg-6 text-center mt-3">
-        <strong>--------------------------------------------------------------------------------------------</strong>
-        <h3><strong>Contact me for further details on all packages and any questions</strong></h3>
-      </div>
-    </div>
-      </div>
-      </div>
 
     <div class="row p-4 p-md-5 text-center text-lg-start shadow-1-strong rounded">
       <div class="d-flex col-12 justify-content-center">
